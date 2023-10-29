8c39704e09327e8eb2e7d03d995422424af31423
Angie <liya0706s@gmail.com>
2023-10-29 20:10:48 +0800

    算出bmi回傳給calc.php

--- calc2.php
+++ calc2.php
@@ -1,13 +1,21 @@
 <?php
+if(!empty($_GET)){
+    $height = (!empty($_GET['height']))?$_GET['height']:'請輸入正確的身高';
+    $weight = (!empty($_GET['weight']))?$_GET['weight']:'請輸入正確的體重';
 
-if (!empty($_GET)) {
-    $height = $_GET['height'];
-    $weight = $_GET['weight'];
+    // 體重（公斤）除以身高（公尺）的平方 
+
+    $bmi=round($weight/($height*$height),2);
 
     echo "身高:" . $height;
+    echo "<br>";
     echo "體重:" . $weight;
+    echo "<br>";
+    echo "BMI值:". $bmi;
+
 }else{
-    echo "請輸入正確資訊";
+    echo "請輸入正確的值";
 }
+    
 
 ?>
